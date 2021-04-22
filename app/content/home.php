573ccd037217f4d9426edb1f9f6860614db6efb7
Bruno Fonseca <brunomf@gmail.com>
2021-04-22 01:58:11 -0300

    customizacao da home

--- app/content/home.php
+++ app/content/home.php
@@ -1,14 +1,11 @@
 <div class="jumbotron">
-    <div class="container"><h1>Welcome to DIWA!</h1>
-        <p><strong>D</strong>eliberately <strong>I</strong>nsecure <strong>W</strong>eb <strong>A</strong>pplication</p>
+    <div class="container"><h1>SIMOX</h1>
     </div>
 </div>
 <div class="row">
     <div class="col-lg-12">
-        <h2>Feel free to hack this site!</h2>
-        <p>This site was made to be hacked. A lot of bugs and vulnerabilities can be exploited to learn how to hack a bad developed website. This
-        should show every developer why it is important to care for website security.</p>
-        <p>DIWA does not guide you through the different attacks or gives you exercises to fulfill. Just try to break everything ;-)</p>
+        <h2>Seja bem-vindo ao nosso site institucional!</h2>
+        <p>Site fictício para utilização em nossos exercícios de cibersegurança.</p>
         <?php if($config['site']['show_extended_homepage']) { ?>
             <h2>How insecure is DIWA?</h2>
             <p>DIWA is horribly insecure! The following vulnerabilities and attack vectors exist in this application:</p>
